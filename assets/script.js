f5804da784d49553129710095e61f707c3ca3a65
Jared <Jared@Littles-MacBook-Pro.local>
2017-12-14 15:38:34 -0500

    added comments

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,4 @@
-
-console.log('hi');
-
+// api key for firebase
 var config = {
     apiKey: "AIzaSyDlhfbcukdhlbrb_4xMJ0kKC3v6M8l-9WQ",
     authDomain: "classdemo-105ab.firebaseapp.com",
@@ -9,7 +7,7 @@ var config = {
     storageBucket: "classdemo-105ab.appspot.com",
     messagingSenderId: "714722563132"
 };
-
+// initialize firebase
 firebase.initializeApp(config);
 
 var database = firebase.database();
@@ -24,12 +22,14 @@ var timeRemaining;
 var minutesTillTrain;
 var newTrainArrival;
 var newTrainArrivalFormatted;
-
+//initialize js
 $(document).ready(function(){
 	function update() {
+//I added a current time clock to the top of the page. I felt like that was a helpful add
 		$('#currentTime').html(moment().format('MMMM Do YYYY, h:mm:ss a'));
 	}
 	setInterval(update, 1000);
+// on click event listener for the add train submit button
 	$("#submit-button").on('click', function(event) {
 		event.preventDefault();
 		$("tbody").empty();
@@ -37,13 +37,16 @@ $(document).ready(function(){
 		newDestination = $("#destination").val().trim();
 		newTrainTime = $("#firstTrainTime").val();	
 		newFrequency = $("#frequency").val();
+// sends info to the database
 		sendToDB();
+// resets the entry form
 		$("#trainName").val("");
   		$("#destination").val("");
   		$("#firstTrainTime").val("");
   		$("#frequency").val("");
 	});
 });
+// sets new info to the database
 var sendToDB = function(){
 	newTrainData = {
 		name: newTrain,
@@ -53,18 +56,21 @@ var sendToDB = function(){
 	};
 	database.ref().push(newTrainData);
 }
+// referencing the database to get values 
 database.ref().on("value", function(snapshot, prevChildKey){
 	for (var i in snapshot.val()){
 		var newTrainName = snapshot.val()[i].name;
 		var newTrainDestination = snapshot.val()[i].destination;
 		var newTrainStart = snapshot.val()[i].startTime;
 		var newTrainFrequency = snapshot.val()[i].frequency;
+// moment.js time math 
 		trainStart = moment(newTrainStart, "hh:mm").subtract(1, "years");
         difference = moment().diff(moment(trainStart), "minutes");
         timeRemaining = difference % newTrainFrequency;
         minutesTillTrain = newTrainFrequency - timeRemaining;
         newTrainArrival = moment().add(minutesTillTrain, "minutes");
         newTrainArrivalFormatted = moment(newTrainArrival).format("hh:mm");
+// appends a new row of train info up on the train schedule table
 		$("tbody").append("<tr><td>" + newTrainName + "</td><td>" + newTrainDestination + "</td><td>" + newTrainFrequency + "</td><td>" + newTrainArrivalFormatted + "</td><td>"+ minutesTillTrain + "</td></tr>");	
 	}
 });
